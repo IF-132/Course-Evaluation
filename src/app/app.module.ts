a7201c78a43c8b4b5d57266b24f98b1068a7bff4
Dmytro Pereklita <74322013+PereDima@users.noreply.github.com>
2021-10-23 14:44:37 +0300

    start making landing hello-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HelloPageComponent } from './hello-page/hello-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloPageComponent
   ],
   imports: [
     BrowserModule,
