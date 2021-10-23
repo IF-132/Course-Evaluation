acc08bc20aeab3d3856f3f3b9caa25b033df7542
Dmytro Pereklita <74322013+PereDima@users.noreply.github.com>
2021-10-23 17:20:37 +0300

    add pictures and styles to hello page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloPageComponent } from './hello-page/hello-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HelloPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HelloPageComponent } from './hello-page/hello-page.component';
+import { FormsModule } from '@angular/forms';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HelloPageComponent } from './hello-page/hello-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
