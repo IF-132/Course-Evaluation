0c549eba8da90220eaf3dad8915e47e52f4f1c37
Yurii Katerlin <yurii.katerlin@gmail.com>
2021-10-24 15:13:26 +0300

    first commit in the feature branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RestorePasswordComponent } from './components/auth/restore-password/restore-password.component';
+import { angularMaterial } from './share/material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestorePasswordComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    angularMaterial
   ],
   providers: [],
   bootstrap: [AppComponent]
