04f5d28cf3aa0bd31da19cb7b7405b8fbfb5c52b
Yurii Katerlin <yurii.katerlin@gmail.com>
2021-10-25 00:05:09 +0300

    added email input in for recovery password

--- src/app/components/auth/restore-password/restore-password.component.ts
+++ src/app/components/auth/restore-password/restore-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-restore-password',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestorePasswordComponent implements OnInit {
 
-  constructor() { }
+    public form: FormGroup | any;;
+    
 
-  ngOnInit(): void {
-  }
+    public ngOnInit(): void {
+      this.form = new FormGroup({
+        email : new FormControl('', [
+          Validators.required,
+          Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"), 
+          Validators.email])
+      })
+      
+    }
 
+    get email() {
+      return this.form.get('email')
+    }
+
+    getEmailError() {
+      return this.email.hasError('required') ? 'You must enter an email address' :
+      this.email.hasError('email') ? 'Not a valid email' : '';
+    }
+    
+    public onSubmit(): void {
+      console.log(this.form)
+    }
 }
+  
--- src/app/share/material/material.module.ts
+++ src/app/share/material/material.module.ts
@@ -1,5 +1,13 @@
-
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatCardModule } from '@angular/material/card'; 
 
 export const angularMaterial = [
-
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
 ]
